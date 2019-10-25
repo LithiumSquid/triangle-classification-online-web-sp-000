dd9dc8fbe0bf6e38fc226ae60361c3fd66f4abc2
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-10-25 19:24:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,13 +23,10 @@ def validate
   if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
         begin
         raise TriangleError
-        rescue TriangleError => error
         puts TriangleError.message
       end
       elsif @side1 + @side2 <= @side3 && @side1 + @side3 <= @side2 && @side2 + @side3 <= @side1
-        begin
         raise TriangleError
-        rescue TriangleError => error
         puts TriangleError.message
       end
     end
